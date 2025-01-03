6773ce2a30b2be12b8900faadebfbff758eec4df
seyren10 <87021985+seyren10@users.noreply.github.com>
2025-01-03 08:41:55 +0800

    Added tailwindcss

---
+++ src/views/Index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="bg-red-500">
+    <p class="text-2xl">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur
+      optio deleniti praesentium facere ab voluptatibus, error fugiat nobis a,
+      iure provident. Ipsum omnis quisquam nobis a fugiat delectus,
+      reiciendisqweqwe atque!
+    </p>
+  </div>
+</template>
+
+<style scoped></style>
