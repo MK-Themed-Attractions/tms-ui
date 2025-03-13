04c033d53398b3be111968db1c768021fb309f91
seyren10 <87021985+seyren10@users.noreply.github.com>
2025-03-13 15:45:55 +0800

    wip: task history index

--- src/pages/task-history/home/Index.vue
+++ src/pages/task-history/home/Index.vue
@@ -1,4 +1,15 @@
 <script setup lang="ts">
+import { InputFilter } from '@/components/app/input-filter';
+import { ref } from 'vue';
+import { inputFilterData } from '.';
+import TaskStepper from '../components/TaskStepper.vue';
+import { Search } from 'lucide-vue-next';
+
+import { useRouteQuery } from '@vueuse/router';
+
+
+const inputFilter = ref(inputFilterData[0])
+const search = useRouteQuery<string>('q')
 
 </script>
 <template>
@@ -6,16 +17,30 @@
         <div>
             <h1 class="text-lg font-semibold md:text-2xl">Task History</h1>
             <p class="text-muted-foreground text-sm">
-                Easily review the progress of your tasks with Task History. This page provides a detailed record of
-                where each task has been, including the departments it has passed through. Track past assignments, see
-                completed work, and stay informed about workflow transitions to ensure transparency and efficiency in
-                task management.
+                Track your task progress with Task History. View past assignments, department transitions, and completed
+                work for transparency and efficiency.
             </p>
         </div>
 
-        <div>
-            
+
+        <div class="rounded-md border p-4 shadow-sm space-y-4">
+            <div class="max-w-md">
+                <InputFilter :dropdown-data="inputFilterData" v-model:filter="inputFilter" v-model:search="search" />
+                <p class="text-xs text-muted-foreground mt-2">View more search options by selecting the dropdown arrow
+                    on the right side of the search input.
+                </p>
+            </div>
+
+            <div v-if="true" class="border border-dashed rounded-md min-h-[50vh] grid place-content-center text-center">
+                <Search class="mx-auto" />
+                <h4 class="text-2xl font-bold">Explore Task History</h4>
+                <p class="text-muted-foreground text-sm">Search for tasks to review their progress and history.</p>
+            </div>
+
+            <TaskStepper v-else />
+
         </div>
+
     </div>
 </template>
 
---
+++ src/pages/task-history/home/index.ts
@@ -0,0 +1,8 @@
+import type { InputFilterDropdownData } from "@/components/app/input-filter";
+
+export const inputFilterData: InputFilterDropdownData[] = [
+  { key: "task-uuid", value: "Task UUID" },
+  { key: "product-sku", value: "Product SKU" },
+  { key: "plan-code", value: "Plan Code" },
+  { key: "batch-uuid", value: "Batch UUID" },
+];
