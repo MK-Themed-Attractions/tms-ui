14f3909faafc2cb1e7113f9c8755044217e15911
seyren10 <87021985+seyren10@users.noreply.github.com>
2025-03-20 08:30:12 +0800

    added: proper description to user management

--- src/pages/user-management/home/Index.vue
+++ src/pages/user-management/home/Index.vue
@@ -229,9 +229,7 @@ await fetchUsers()
         <div>
             <h1 class="text-lg font-semibold md:text-2xl">User management</h1>
             <p class="text-muted-foreground">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis amet explicabo pariatur rerum
-                recusandae veritatis, incidunt suscipit praesentium? Dicta omnis laboriosam quos ullam atque expedita
-                consequatur sit, in amet eum!
+                Manage users, assign roles and permissions, edit details, and reset passwords with ease.
             </p>
         </div>
 
