d7076c3e039887c440429372e876c123181a4a05
seyren10 <87021985+seyren10@users.noreply.github.com>
2025-03-24 13:40:11 +0800

    added: initial message on home page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,16 +3,22 @@ import { ImageApp } from "@/components/app/image";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { msTeamLink } from "@/lib/utils";
+import { useAuthStore } from "@/stores/authStore";
+import { Hand, PartyPopper } from "lucide-vue-next";
+import { storeToRefs } from "pinia";
+
+const authStore = useAuthStore()
+const { user } = storeToRefs(authStore)
 </script>
 <template>
   <div class="flex items-center">
     <h1 class="text-lg font-semibold md:text-2xl">MKTA Task management system</h1>
   </div>
   <div class="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm">
-    <div class="flex flex-col items-center gap-1 text-center">
+    <div class="flex flex-col items-center gap-1 text-center" v-if="!user">
       <ImageApp image="/MKLogo-White.svg" class="invert size-[8rem]" />
       <h3 class="text-2xl font-bold tracking-tight">Login Required</h3>
-      <p class="text-sm text-muted-foreground">
+      <p class="text-sm text-muted-foreground max-w-[27ch]">
         Start using TMS by logging in. Don&apos;t have an account? Contact the <a class="font-medium"
           :href="msTeamLink()">Dev team</a>.
       </p>
@@ -23,7 +29,15 @@ import { msTeamLink } from "@/lib/utils";
         </RouterLink>
       </Button>
     </div>
+    <div v-else>
+      <div class="font-bold flex gap-2 items-center justify-center">
+        <span> Welcome ,{{ user.given_name }} </span>
+        <PartyPopper />
+      </div>
+      <p class="text-sm">Dashboard feature will be added soon...</p>
+    </div>
   </div>
+
 </template>
 
 <style scoped></style>
