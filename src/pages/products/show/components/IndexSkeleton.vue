bbbc7bc288e5fcf148e98ea4f14743154d7d8bfd
seyren10 <87021985+seyren10@users.noreply.github.com>
2025-03-29 10:47:19 +0800

    added: skeleton loader for showing individual products

---
+++ src/pages/products/show/components/IndexSkeleton.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="grid gap-4 px-4 lg:grid-cols-2">
+        <div class="rounded-md bg-muted min-h-[27rem]">
+        </div>
+        <div class="rounded-md bg-muted min-h-[27rem]">
+        </div>
+        <div class="rounded-md bg-muted min-h-[20rem] col-span-full">
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
