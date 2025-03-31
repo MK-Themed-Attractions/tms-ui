f0fd53fa5d02e90bffdbfc34259cd702ecf0019a
seyren10 <87021985+seyren10@users.noreply.github.com>
2025-03-31 12:16:31 +0800

    optimized: searching and product view

--- src/pages/products/show/components/IndexSkeleton.vue
+++ src/pages/products/show/components/IndexSkeleton.vue
@@ -1,15 +1,17 @@
 <template>
     <div class="grid gap-4 px-4 lg:grid-cols-2">
-        <div class="rounded-md bg-muted min-h-[27rem]">
-        </div>
-        <div class="rounded-md bg-muted min-h-[27rem]">
-        </div>
-        <div class="rounded-md bg-muted min-h-[20rem] col-span-full">
-        </div>
+        <Skeleton class="min-h-[27rem]">
+        </Skeleton>
+        <Skeleton class="min-h-[27rem]">
+        </Skeleton>
+        <Skeleton class="min-h-[20rem] col-span-full">
+        </Skeleton>
     </div>
 </template>
 
 <script setup lang="ts">
+import { Skeleton } from '@/components/ui/skeleton';
+
 
 </script>
 
