0ec92e79d951d4a176b447a158fbd8d70ed8b688
seyren10 <87021985+seyren10@users.noreply.github.com>
2025-05-20 13:50:07 +0800

    added: template for custom label

---
+++ src/pages/products/show/components/ProductCustomLabel.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+
+
+</script>
+<template>
+    <Card>
+        <CardHeader>
+            <CardTitle class="text-lg">
+                Custom Label
+            </CardTitle>
+            <CardDescription>Print this product custom label</CardDescription>
+        </CardHeader>
+        <CardContent>
+            <!-- YOUR CODE GOES HERE -->
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum error, recusandae tenetur voluptates modi
+            eius consequatur architecto explicabo laborum est quo! Est eum deleniti vitae asperiores debitis molestias
+            modi tempore.
+        </CardContent>
+    </Card>
+</template>
+
+
+
+<style scoped></style>
